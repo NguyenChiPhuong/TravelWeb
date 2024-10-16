e95c7c84047e4042e2f0387ebdbf3a0ee93845de
unknown <badwangyibo@gmail.com>
2024-10-16 16:56:01 +0700

    flask

--- chinese_travel/app.py
+++ chinese_travel/app.py
@@ -1,18 +1,59 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', active_page='home')
 
 @app.route('/destinations')
 def destinations():
-    return render_template('destinations.html')
+    return render_template('destinations.html', active_page='destinations')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', active_page='contact')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            flash('Login successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed. Check your username and/or password.', 'danger')
+    return render_template('login.html', active_page='login')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        new_user = User(username=username, password=hashed_password)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Sign up successful! You can now log in.', 'success')
+            return redirect(url_for('login'))
+        except:
+            flash('Sign up failed. Username may already be taken.', 'danger')
+    return render_template('signup.html', active_page='signup')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
