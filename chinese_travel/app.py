4592e2e66798f97531a27f4e633cbf19be7bbd89
unknown <badwangyibo@gmail.com>
2024-10-17 13:55:24 +0700

    C

--- chinese_travel/app.py
+++ chinese_travel/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, session, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -31,27 +31,41 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
         if user and check_password_hash(user.password, password):
-            flash('Login successful!', 'success')
-            return redirect(url_for('home'))
+            session['user_id'] = user.id
+            flash(f'Xin chào {username}!', 'success')
+            return redirect(url_for('index'))
         else:
-            flash('Login failed. Check your username and/or password.', 'danger')
-    return render_template('login.html', active_page='login')
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html')
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        hashed_password = generate_password_hash(password, method='sha256')
-        new_user = User(username=username, password=hashed_password)
-        try:
+        confirm_password = request.form['confirm_password']
+        if password != confirm_password:
+            flash('Passwords do not match', 'danger')
+        else:
+            hashed_password = generate_password_hash(password, method='sha256')
+            new_user = User(username=username, password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
-            flash('Sign up successful! You can now log in.', 'success')
+            flash('Bạn đã đăng ký thành công!', 'success')
             return redirect(url_for('login'))
-        except:
-            flash('Sign up failed. Username may already be taken.', 'danger')
-    return render_template('signup.html', active_page='signup')
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('index'))
+
+@app.route('/')
+def index():
+    user_id = session.get('user_id')
+    user = User.query.filter_by(id=user_id).first() if user_id else None
+    return render_template('index.html', user=user)
 
 if __name__ == '__main__':
     with app.app_context():
